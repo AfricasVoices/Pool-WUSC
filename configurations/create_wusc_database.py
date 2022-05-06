e5d363e2a3d77a4456ce14948ae8eacc307ef5c4
IsaackMwenda <mwendaisaac@gmail.com>
2022-05-06 11:47:11 +0300

    move old rqa messages to kakuma_old_rqa_datasets

--- configurations/create_wusc_database.py
+++ configurations/create_wusc_database.py
@@ -104,6 +104,14 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
                     ],
                     ws_code_string_value="kakuma age"
                 ),
+                CodaDatasetConfiguration(
+                    coda_dataset_id="WUSC_kakuma_old_rqa_datasets",
+                    engagement_db_dataset="kakuma_old_rqa_datasets",
+                    code_scheme_configurations=[
+                        CodeSchemeConfiguration(code_scheme=load_code_scheme("kakuma_old_rqa_datasets"), auto_coder=None)
+                    ],
+                    ws_code_string_value="kakuma old rqa datasets"
+                ),
             ],
             ws_correct_dataset_code_scheme=load_code_scheme("ws_correct_dataset"),
             project_users_file_url="gs://avf-project-datasets/2021/WUSC-LEAP/coda_users.json"
