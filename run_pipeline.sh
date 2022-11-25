2bedfc13790c8db23eaf4a46d588329406ec2684
IsaackMwenda <mwendaisaac@gmail.com>
2022-11-26 00:54:11 +0300

    standardise pipeline run scripts

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ $# -ne 7 ]]; then
-    echo "Usage: ./run_sms_pipeline.sh"
+    echo "Usage: ./run_pipeline.sh"
     echo "<user> <pipeline-name> <google-cloud-credentials-file-path> <configuration-file> <code-schemes-dir> <data-dir> <archive-dir>"
     echo "Runs the pipeline end-to-end (sync-rapid-pro-to-engagement-db, sync-engagement-db-to-coda, sync-coda-to-engagement-db,\
           sync-engagement-db-to-rapid-pro, run-engagement-db-to-analysis, ARCHIVE)"
