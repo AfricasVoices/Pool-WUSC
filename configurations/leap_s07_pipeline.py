0576f51b3cdcc4cf85f990cd8f60b8858e805a46
Daniel Mwendwa <danielmwendwam@gmail.com>
2024-08-20 12:56:06 +0300

    wusc kobo config

--- configurations/leap_s07_pipeline.py
+++ configurations/leap_s07_pipeline.py
@@ -116,6 +116,20 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
                 ] + demogs_question_configurations
             )
         ),
+        KoboToolBoxSource(
+            token_file_url="gs://avf-credentials/uraia-kobotoolbox-token.json",
+            sync_config=KoboToolBoxToEngagementDBConfiguration(
+                asset_uid="abm3DokhUSqUfrPRXgJjKZ",
+                participant_id_configuration=KoboToolBoxParticipantIdConfiguration(
+                    data_column_name="Contacts",
+                    id_type=KoboToolBoxParticipantIdTypes.KENYA_MOBILE_NUMBER
+                ),
+                ignore_invalid_mobile_numbers=True,
+                question_configurations=[
+                    KoboToolBoxQuestionConfiguration(data_column_name="leap_s07e04", engagement_db_dataset="leap_s07e04"),
+                ] + demogs_question_configurations
+            )
+        ),
     ],
     coda_sync=CodaConfiguration(
         coda=CodaClientConfiguration(credentials_file_url="gs://avf-credentials/coda-production.json"),
