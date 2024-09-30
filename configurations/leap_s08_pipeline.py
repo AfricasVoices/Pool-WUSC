b46a21acb2c44bac18abbee993a94666130483ce
Daniel Mwendwa <danielmwendwam@gmail.com>
2024-09-30 15:40:43 +0300

    leap s08e03 Kobo Config

--- configurations/leap_s08_pipeline.py
+++ configurations/leap_s08_pipeline.py
@@ -78,20 +78,20 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
                 ] + demogs_question_configurations
             )
         ),
-        # KoboToolBoxSource(
-        #     token_file_url="gs://avf-credentials/uraia-kobotoolbox-token.json",
-        #     sync_config=KoboToolBoxToEngagementDBConfiguration(
-        #         asset_uid="",
-        #         participant_id_configuration=KoboToolBoxParticipantIdConfiguration(
-        #             data_column_name="Contacts",
-        #             id_type=KoboToolBoxParticipantIdTypes.KENYA_MOBILE_NUMBER
-        #         ),
-        #         ignore_invalid_mobile_numbers=True,
-        #         question_configurations=[
-        #             KoboToolBoxQuestionConfiguration(data_column_name="leap_s08e03", engagement_db_dataset="leap_s08e03"),
-        #         ] + demogs_question_configurations
-        #     )
-        # ),
+        KoboToolBoxSource(
+            token_file_url="gs://avf-credentials/uraia-kobotoolbox-token.json",
+            sync_config=KoboToolBoxToEngagementDBConfiguration(
+                asset_uid="azNLcrQYCSxafyknKbQzvs",
+                participant_id_configuration=KoboToolBoxParticipantIdConfiguration(
+                    data_column_name="Contacts",
+                    id_type=KoboToolBoxParticipantIdTypes.KENYA_MOBILE_NUMBER
+                ),
+                ignore_invalid_mobile_numbers=True,
+                question_configurations=[
+                    KoboToolBoxQuestionConfiguration(data_column_name="leap_s08e03", engagement_db_dataset="leap_s08e03"),
+                ] + demogs_question_configurations
+            )
+        ),
         # KoboToolBoxSource(
         #     token_file_url="gs://avf-credentials/uraia-kobotoolbox-token.json",
         #     sync_config=KoboToolBoxToEngagementDBConfiguration(
