383c856660739ba08cdfc972e0c967af45ea9cab
Daniel Mwendwa <danielmwendwam@gmail.com>
2024-10-08 13:02:14 +0300

    update flow names

--- configurations/leap_s08_pipeline.py
+++ configurations/leap_s08_pipeline.py
@@ -35,8 +35,8 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
                     FlowResultConfiguration("wusc_leap_s08e01_activation", "rqa_s08e01", "leap_s08e01"),
                     FlowResultConfiguration("wusc_leap_s08e02_activation", "rqa_s08e02", "leap_s08e02"),
                     FlowResultConfiguration("wusc_leap_s08e03_activation", "rqa_s08e03", "leap_s08e03"),
-                    FlowResultConfiguration("wusc_leap_s08e04_activation", "rqa_s08e04", "leap_s08e04"),
-                    FlowResultConfiguration("wusc_leap_s08e05_activation", "rqa_s08e05", "leap_s08e05"),
+                    FlowResultConfiguration("wusc_leap_s08e4_activation", "rqa_s08e04", "leap_s08e04"),
+                    FlowResultConfiguration("wusc_leap_s08e5_activation", "rqa_s08e05", "leap_s08e05"),
                     # FlowResultConfiguration("wusc_leap_s08e06_activation", "rqa_s08e06", "leap_s08e06"),
 
                     FlowResultConfiguration("wusc_leap_s07_demogs", "age", "kakuma_age"),
