4a052b6f1d770365ba6ca538b576e679f056c1b4
Daniel Mwendwa <danielmwendwam@gmail.com>
2024-10-11 12:21:52 +0300

    kobo s08e05 config

--- configurations/leap_s08_pipeline.py
+++ configurations/leap_s08_pipeline.py
@@ -106,20 +106,20 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
                 ] + demogs_question_configurations
             )
         ),
-        # KoboToolBoxSource(
-        #     token_file_url="gs://avf-credentials/uraia-kobotoolbox-token.json",
-        #     sync_config=KoboToolBoxToEngagementDBConfiguration(
-        #         asset_uid="",
-        #         participant_id_configuration=KoboToolBoxParticipantIdConfiguration(
-        #             data_column_name="Contacts",
-        #             id_type=KoboToolBoxParticipantIdTypes.KENYA_MOBILE_NUMBER
-        #         ),
-        #         ignore_invalid_mobile_numbers=True,
-        #         question_configurations=[
-        #             KoboToolBoxQuestionConfiguration(data_column_name="leap_s08e05", engagement_db_dataset="leap_s08e05"),
-        #         ] + demogs_question_configurations
-        #     )
-        # ),
+        KoboToolBoxSource(
+            token_file_url="gs://avf-credentials/uraia-kobotoolbox-token.json",
+            sync_config=KoboToolBoxToEngagementDBConfiguration(
+                asset_uid="aCjy76uHNNTr6gYc7Pcdrt",
+                participant_id_configuration=KoboToolBoxParticipantIdConfiguration(
+                    data_column_name="Contacts",
+                    id_type=KoboToolBoxParticipantIdTypes.KENYA_MOBILE_NUMBER
+                ),
+                ignore_invalid_mobile_numbers=True,
+                question_configurations=[
+                    KoboToolBoxQuestionConfiguration(data_column_name="leap_s08e05", engagement_db_dataset="leap_s08e05"),
+                ] + demogs_question_configurations
+            )
+        ),
         # KoboToolBoxSource(
         #     token_file_url="gs://avf-credentials/uraia-kobotoolbox-token.json",
         #     sync_config=KoboToolBoxToEngagementDBConfiguration(
